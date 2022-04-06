30b6bfb4ca1c6e4d99973808429c4075399e4540
saehee15 <sae_hwi@naver.com>
2022-04-06 23:01:53 +0900

    20200406_첫번째 커밋

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    //cursor
+    $('body').mousemove(function(e){
+        xVal = e.pageX - $('.cursor').width()/2;
+        yVal = e.pageY- $('.cursor').height()/2;
+
+        gsap.to('.cursor',{
+            x: xVal,
+            y: yVal
+        })
+    })
+
+
+    
+    $('.btn > div').click(function () { 
+        var hasOn = $(this).hasClass('on')
+
+        if(hasOn){
+            $(this).removeClass('on')
+            $(this).siblings().addClass('on')
+            $('.message').css("opacity", "1")
+           
+        } else {
+            $(this).addClass('on')
+            $(this).siblings().removeClass('on')
+        }
+    });
+
+})
