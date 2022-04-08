23deab01131e41508ad1ad449152f78e508f3d43
saehee15 <sae_hwi@naver.com>
2022-04-08 22:55:02 +0900

    20200408_css 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    //cursor
+    // cursor
     $('body').mousemove(function(e){
         xVal = e.pageX - $('.cursor').width()/2;
         yVal = e.pageY- $('.cursor').height()/2;
@@ -10,23 +10,64 @@ $(function(){
         })
     })
 
-    $('.navbar').click(function(){
-        ('.nav_main').css("opacity", "1")
+    const txt = gsap.timeline({})
+    txt.fromTo('.section1 .text_animation div', {
+            y: 0,
+            duration: 0.1,
+            stagger: 1,
+        },{
+            y: -100,
+            duration: 0.1,
+            stagger: 1,
+        }
+         )
+
+
+
+    var txtMotion = gsap.fromTo('.section2 .text_animation',{
+        yPercent: 100,
+    },{
+        yPercent: 0,
+        stagger: 0.2
+    })
+    
+    ScrollTrigger.create({
+        trigger:".section2",
+        start:"top 100%",
+        onEnter:function(){
+            $('.img_container').addClass('on')
+            txtMotion
+        }
     })
 
-    
-    $('.btn > div').click(function () { 
-        var hasOn = $(this).hasClass('on')
-
+    $(document).on("click", ".navbar", function () {
+        var hasOn = $('.menu_close').hasClass('hide')
+        console.log(hasOn)
         if(hasOn){
-            $(this).removeClass('on')
-            $(this).siblings().addClass('on')
-            $('.message').css("opacity", "1")
+            $('.menu_open').addClass('hide')
+            $('.menu_close').removeClass('hide')  
+            $('.nav_main').addClass('on')
            
         } else {
-            $(this).addClass('on')
-            $(this).siblings().removeClass('on')
+            $('.menu_open').removeClass('hide')
+            $('.menu_close').addClass('hide')
+            $('.nav_main').removeClass('on')   
         }
-    });
+      });
 
+    $(document).on("click", ".btn div", function () {
+        var hasOn = $('.reservation_btn').hasClass('on')
+        console.log(hasOn)
+        if(hasOn){
+            $('.reservation_btn').removeClass('on')
+            $('.close_btn').addClass('on')  
+            $('.message').addClass('on')
+           
+        } else {
+            $('.reservation_btn').addClass('on')
+            $('.close_btn').removeClass('on')
+            $('.message').removeClass('on')
+            
+        }
+      });
 })
