63e24d64f0fd0684491cc285492aa378c3971f54
saehee15 <sae_hwi@naver.com>
2022-04-15 17:56:20 +0900

    20220415_반응형웹

--- js/main.js
+++ js/main.js
@@ -12,10 +12,10 @@ $(function(){
         stagger:0.05
     })
 
+    if (matchMedia("screen and (max-width: 767px)").matches) 
+         {var slide = new Swiper(".slide", {
 
-    var slide = new Swiper(".slide", {
-
-        slidesPerView: '3',
+        slidesPerView: '2',
         effect: 'coverflow',
         coverflowEffect: {
             rotate: 0,
@@ -26,108 +26,38 @@ $(function(){
         loop: true,
         autoplay: true,
         centeredSlides: true,
+    })}
+        else {   var slide = new Swiper(".slide", {
 
-        // pagination: {
-        //     el: ".swiper-pagination",
-        //     clickable: true,
-        //     renderBullet: function (index, className) {
-        //       return '<span class="' + className + '">' + "</span>";
-        // }}
+            slidesPerView: '3',
+            effect: 'coverflow',
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 10,
+                slideShadows: false,
+              },
+            speed: 1000,
+            loop: true,
+            autoplay: true,
+            centeredSlides: true,
+        }) }
 
-        
-    });
 
-    $(document).ready(function() {
-        var containers = $('.container');
-    
-        if (containers.length) {
-            containers.each(function() {
-                var container = $(this);
-    
-                // Support small text - copy to fill screen width
-                if (container.find('.scrolling-text').outerWidth() < $(window).width()) {
-                    var windowToScrolltextRatio = Math.round($(window).width() / container.find('.scrolling-text').outerWidth()),
-                        scrollTextContent = container.find('.scrolling-text .scrolling-text-content').text(),
-                        newScrollText = '';
-                    for (var i = 0; i < windowToScrolltextRatio; i++) {
-                        newScrollText += ' ' + scrollTextContent;
-                    }
-                    container.find('.scrolling-text .scrolling-text-content').text(newScrollText);
-                }
-    
-                // Init variables and config
-                var scrollingText = container.find('.scrolling-text'),
-                    scrollingTextWidth = scrollingText.outerWidth(),
-                    scrollingTextHeight = scrollingText.outerHeight(true),
-                    startLetterIndent = parseInt(scrollingText.find('.scrolling-text-content').css('font-size'), 10) / 4.8,
-                    startLetterIndent = Math.round(startLetterIndent),
-                    scrollAmountBoundary = Math.abs($(window).width() - scrollingTextWidth),
-                    transformAmount = 0,
-                    leftBound = 0,
-                    rightBound = scrollAmountBoundary,
-                    transformDirection = container.hasClass('left-to-right') ? -1 : 1,
-                    transformSpeed = 200;
-    
-                // Read transform speed
-                if (container.attr('speed')) {
-                    transformSpeed = container.attr('speed');
-                }
-            
-                // Make scrolling text copy for scrolling infinity
-                container.append(scrollingText.clone().addClass('scrolling-text-copy'));
-                container.find('.scrolling-text').css({'position': 'absolute', 'left': 0});
-                container.css('height', scrollingTextHeight);
-            
-                var getActiveScrollingText = function(direction) {
-                    var firstScrollingText = container.find('.scrolling-text:nth-child(1)');
-                    var secondScrollingText = container.find('.scrolling-text:nth-child(2)');
-            
-                    var firstScrollingTextLeft = parseInt(container.find('.scrolling-text:nth-child(1)').css("left"), 10);
-                    var secondScrollingTextLeft = parseInt(container.find('.scrolling-text:nth-child(2)').css("left"), 10);
-            
-                    if (direction === 'left') {
-                        return firstScrollingTextLeft < secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
-                    } else if (direction === 'right') {
-                        return firstScrollingTextLeft > secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
-                    }
-                }
-            
-                $(window).on('wheel', function(e) {
-                    var delta = e.originalEvent.deltaY;
-                    
-                    if (delta > 0) {
-                        // going down
-                        transformAmount += transformSpeed * transformDirection;
-                        container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(10deg)');
-                    }
-                    else {
-                        transformAmount -= transformSpeed * transformDirection;
-                        container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(-10deg)');
-                    }
-                    setTimeout(function(){
-                        container.find('.scrolling-text').css('transform', 'translate3d('+ transformAmount * -1 +'px, 0, 0)');
-                    }, 10);
-                    setTimeout(function() {
-                        container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(0)');
-                    }, 500)
-            
-                    // Boundaries
-                    if (transformAmount < leftBound) {
-                        var activeText = getActiveScrollingText('left');
-                        activeText.css({'left': Math.round(leftBound - scrollingTextWidth - startLetterIndent) + 'px'});
-                        leftBound = parseInt(activeText.css("left"), 10);
-                        rightBound = leftBound + scrollingTextWidth + scrollAmountBoundary + startLetterIndent;
-            
-                    } else if (transformAmount > rightBound) {
-                        var activeText = getActiveScrollingText('right');
-                        activeText.css({'left': Math.round(rightBound + scrollingTextWidth - scrollAmountBoundary + startLetterIndent) + 'px'});
-                        rightBound += scrollingTextWidth + startLetterIndent;
-                        leftBound = rightBound - scrollingTextWidth - scrollAmountBoundary - startLetterIndent;
-                    }
-                });
-            })
-        }
-    });
+    //flip
+    gsap.registerPlugin(Flip);
+    const squares = gsap.utils.toArray(".txt");
+ 
+    function doFlip() {
+    const state = Flip.getState(squares);
+    swap(squares);
+    Flip.from(state, {duration: 2, ease: "power1.inOut"});
+    }
+
+    function swap([a, b]) {
+     a.parentNode.children[0] === a ? a.parentNode.appendChild(a) : a.parentNode.appendChild(b);
+    }
+
+    document.addEventListener("scroll", doFlip);
 
 
 
@@ -147,7 +77,6 @@ $(function(){
         animation: prdMotion,
         trigger:".section2",
         start:"top 80%",
-        markers: true
     })
 
     
@@ -166,7 +95,6 @@ $(function(){
         animation: motion,
         trigger:".section4",
         start:"top 80%",
-        markers: true
     })
    
    
@@ -183,33 +111,12 @@ $(function(){
             scrollTrigger:{
                 trigger:el,
                 start:"top 80%",
-                markers:true,
             },
             scale:1,
             opacity: 1,
             filter: 'blur(0px)'
         })
     });
-
-
-
-    $(document).on("click", ".navbar", function () {
-        var hasOn = $('.menu_close').hasClass('hide')
-        console.log(hasOn)
-        if(hasOn){
-            
-            $('body').addClass('hidden')
-            $('.menu_open').addClass('hide')
-            $('.menu_close').removeClass('hide')  
-            $('.nav_main').addClass('on')
-           
-        } else {
-            $('body').removeClass('hidden')
-            $('.menu_open').removeClass('hide')
-            $('.menu_close').addClass('hide')
-            $('.nav_main').removeClass('on')   
-        }
-      });
 
 
       const prdTitle = new SplitType('.section3 h2 .txt_hide', { types: 'words, chars', });
@@ -222,7 +129,6 @@ $(function(){
         scrollTrigger:{
             trigger:".section2",
             start:"top 80%",
-            markers: true
         },
           opacity: 1,
           yPercent: 0,
